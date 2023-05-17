eb26e992751cd029091e4bfae0991f70ab4f0a2f
happylisaya <Lisalin829@gmail.com>
2023-05-17 11:22:31 +0800

    week14_挑選關節

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -5,12 +5,13 @@ GLMmodel * head = NULL;
 GLMmodel * body = NULL;
 GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
-int show[4] = {0, 1, 0, 0};
+int show[4] = {1, 1, 1, 1};
+int ID=0;
 void keyboard(unsigned char key, int x, int y) {
-    if(key=='0') show[0] = !show[0];
-    if(key=='1') show[1] = !show[1];
-    if(key=='2') show[2] = !show[2];
-    if(key=='3') show[3] = !show[3];
+    if(key=='0') ID = 0;
+    if(key=='1') ID = 1;
+    if(key=='2') ID = 2;
+    if(key=='3') ID = 3;
     glutPostRedisplay();
 }
 FILE * fout = NULL;
@@ -22,23 +23,31 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
         glScalef(0.2, 0.2, 0.2);
-        if(body==NULL){///week13 step02-1
+        if(body==NULL){
             head = glmReadOBJ("model/head.obj");
             body = glmReadOBJ("model/body.obj");
             uparmR = glmReadOBJ("model/uparmR.obj");
             lowarmR = glmReadOBJ("model/lowarmR.obj");
         }
-        glColor3f(1,0,0);
+        if(ID==0) glColor3f(1, 0, 0);
+        else glColor3f(1,1,1);
         if(show[0]) glmDraw(head, GLM_MATERIAL);
-        glColor3f(0,1,0);
-        if(show[1]) glmDraw(body, GLM_MATERIAL);
-        glPushMatrix();
-            glTranslatef(teapotX, teapotY, 0);
 
-            if(show[2]) glmDraw(uparmR, GLM_MATERIAL);
-        glPopMatrix();
+        if(ID==1) glColor3f(1, 0, 0);
+        else glColor3f(1,1,1);
+        if(show[1]) glmDraw(body, GLM_MATERIAL);
+
+        if(ID==2) glColor3f(1, 0, 0);
+        else glColor3f(1,1,1);
+        if(show[2]) glmDraw(uparmR, GLM_MATERIAL);
+
+        if(ID==3) glColor3f(1, 0, 0);
+        else glColor3f(1,1,1);
         if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);
-    glPopMatrix();
+        glTranslated(teapotX, teapotY, 0);
+
+        glPopMatrix();
+        glPopMatrix();
     glutSwapBuffers();
 }
 int oldX = 0, oldY = 0;
@@ -59,15 +68,7 @@ void mouse(int button, int state, int x, int y)
     }
     display();
 }
-//void keyboard(unsigned char key, int x, int y) ///step02-2 keyboard�禡
-//{
-//    if(fin==NULL){ ///step02-2 �p�G�ɮ��٨S fopen(), �N�}��
-//        fclose(fout); ///�e��mouse�|�}fout����, �ҥH�n����
-//        fin = fopen("file4.txt", "r"); ///step02-2 �S�}��,�N�}
-//    }
-//    ///fscanf(fin, "%f %f", &teapotX, &teapotY); ///step02-2 �u��Ū��
-//    display(); ///step02-2 ���e�e��
-//}
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
