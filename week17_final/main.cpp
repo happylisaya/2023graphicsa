bfdde9246e9b32bcc548dbf71be52b36bd5fd552
happylisaya <Lisalin829@gmail.com>
2023-06-06 09:33:42 +0800

    add week17

--- week17_final/main.cpp
+++ week17_final/main.cpp
@@ -3,6 +3,49 @@
 #include <opencv/cv.h>
 #include <GL/glut.h>
 #include "glm.h"
+#include <GL/glut.h>
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+void myLight()
+{
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+}
+
+float angle = 0; ///step02-1 宣告global全域變數 angle
+void display()
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ); ///清背景
+    glPushMatrix(); ///step02-1 備份矩陣
+        glRotatef(angle, 0, 1, 0); ///step02-1 旋轉 angle 角度
+        glutSolidTeapot( 0.3 );
+    glPopMatrix();  ///step02-1 還原矩陣
+    glutSwapBuffers();
+    angle++; ///step02-1 把角度++
+}
 
 GLMmodel * head = NULL;
 GLMmodel * body = NULL;
@@ -15,7 +58,7 @@ GLMmodel * right_foot = NULL;
 int myTexture(char * filename)
 {
     IplImage * img = cvLoadImage(filename);
-    cvCvtColor(img,img, CV_BGR2RGB);
+    cvCvtColor(img, img, CV_BGR2RGB);
     glEnable(GL_TEXTURE_2D);
     GLuint id;
     glGenTextures(1, &id);
@@ -104,7 +147,7 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
         glPushMatrix();
             glScalef(6, 6, 6);
-            glTranslatef(0 , -0.05, 0);
+            glTranslatef(0 , -0.1, 0);
             glPushMatrix();
                 glColor3f(1,1,1);
                 glScalef(0.03, 0.03, 0.03);
@@ -197,8 +240,8 @@ int main(int argc, char** argv)
     left_foot = glmReadOBJ("model/left_foot.obj");
     right_foot = glmReadOBJ("model/right_foot.obj");
 
-    myTexture("model/color2.jpg");
-    glEnable(GL_DEPTH_TEST);
+    //glEnable(GL_DEPTH_TEST);
 
+    myLight(); ///step02-2 我們自己寫一個 myLight()函式, 裡面有很多行!
     glutMainLoop();
 }
